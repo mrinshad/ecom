4f032163f5ed235edfc40cbdf19ca85c9a64cd31
MRinshad <rinshadmorayur09@gmail.com>
2022-06-26 19:08:13 +0530

    some changes to gradle

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
